7beaff36de60c8b522cad4edb84b3ebe5c960151
777yur0k <56153873+777yur0k@users.noreply.github.com>
2024-07-29 15:40:00 +0300

    Attack&Movement imp.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,6 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float speed = 5f;
     Vector2 moveDelta;
     public GameObject attack, sword;
-    Vector3 attackPosition = new(0.6f, 0.0f, 0.0f), attackFlippedPosition = new(-0.6f, 0.0f, 0.0f), swordPosition = new(0.3f, 0.1f, 0.0f), swordFlippedPosition = new(-0.3f, 0.1f, 0.0f);
     [SerializeField] bool startFlipped;
     public GameObject Key;
 
@@ -15,46 +14,23 @@ public class PlayerController : MonoBehaviour
     {
         rbody = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-
-        if (startFlipped) setFlipped();
-        else setUnflipped();
     }
 
     void Update()
     {
-        calculateMove();
-        verifyFlip();
+        CalculateMove();
     }
 
     public void ShowKey() => Key.SetActive(true);
 
-    void calculateMove()
+    void CalculateMove()
     {
         moveDelta.x = Input.GetAxisRaw("Horizontal");
         moveDelta.y = Input.GetAxisRaw("Vertical");
-    }
 
-    void setFlipped()
-    {
-        spriteRenderer.flipX = true;
-        attack.transform.localPosition = attackFlippedPosition;
-        sword.transform.localPosition = swordFlippedPosition;
-        sword.GetComponent<Weapon>().setFlipped(true);
+        if (moveDelta.x < 0) transform.localScale = new(1,1,1);
+        else if (moveDelta.x > 0) transform.localScale = new(-1, 1, 1);
     }
-
-    void setUnflipped()
-    {
-        spriteRenderer.flipX = false;
-        attack.transform.localPosition = attackPosition;
-        sword.transform.localPosition = swordPosition;
-        sword.GetComponent<Weapon>().setFlipped(false);
-    }
-
-    void verifyFlip()
-    {
-        if (moveDelta.x < 0) setFlipped();
-        else if (moveDelta.x > 0) setUnflipped();
-    }
-
+    
     void FixedUpdate() => rbody.MovePosition(rbody.position + (moveDelta.normalized * speed * Time.fixedDeltaTime));
 }
